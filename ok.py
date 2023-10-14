e456c54c0085d82dae250775edc5241beb622431
Isha-Sriv <isrivastava4@wisc.edu>
2023-10-14 14:00:32 -0500

    grinning emoji

---
+++ ok.py
@@ -0,0 +1,41 @@
+from turtle import *
+
+speed(0)
+
+def ok():
+    fillcolor("yellow")
+    begin_fill()
+    circle(30)
+    end_fill()
+    
+    color("black")
+    penup()
+    left(90)
+    forward(35)
+    left(90)
+    forward(15)
+    right(180)
+    pendown()
+    
+    forward(15)
+    penup()
+    forward(5)
+    pendown()
+    forward(15)
+    
+    penup()
+    right(90)
+    forward(15)
+    right(90)
+    forward(10)
+    pendown()
+    forward(20)
+    
+    penup()
+    left(90)
+    forward(20)
+    left(90)
+    forward(10)
+    
+ok()
+done()
